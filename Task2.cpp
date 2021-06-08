2897ac35b523d2e5785532e6057322cc44badb29
alexberkov <82799873+alexberkov@users.noreply.github.com>
2021-06-08 16:19:21 +0300

    Add files via upload

--- Task2.cpp
+++ Task2.cpp
@@ -6,6 +6,20 @@
 
 using namespace std;
 
+typedef unsigned long ul;
+
+bool isEqual (tm date, tm birthday) {
+  return (date.tm_mon == birthday.tm_mon && date.tm_mday == birthday.tm_mday);
+}
+
+bool isMore (tm date, tm today) {
+  return (date.tm_mon > today.tm_mon || (date.tm_mon == today.tm_mon && date.tm_mday > today.tm_mday));
+}
+
+bool isLess (tm date, tm birthday) {
+  return (date.tm_mon < birthday.tm_mon || (date.tm_mon == birthday.tm_mon && date.tm_mday < birthday.tm_mday));
+}
+
 int main() {
   string line, name;
   time_t t = time(nullptr);
@@ -15,25 +29,34 @@ int main() {
   birthday.tm_mon = 11;
   birthday.tm_mday = 31;
   vector<string> bornToday;
+  vector<pair<string,tm>> closest;
   while (line != "end") {
     cout << "Enter name: ";
     cin >> line;
     if (line == "end") continue;
     cout << "Enter date of birth: ";
     cin >> get_time(&date, "%Y/%m/%d");
-    if (date.tm_mon == today.tm_mon && date.tm_mday == today.tm_mday) bornToday.push_back(line);
-    if (date.tm_mon > today.tm_mon || (date.tm_mon == today.tm_mon && date.tm_mday > today.tm_mday)) {
-      if (date.tm_mon < birthday.tm_mon || (date.tm_mon == birthday.tm_mon && date.tm_mday < birthday.tm_mday)) {
+    if (isEqual(date, today)) bornToday.push_back(line);
+    if (isMore(date, today)) {
+      if (isLess(date, birthday) || isEqual(date, birthday)) {
         birthday.tm_mon = date.tm_mon;
         birthday.tm_mday = date.tm_mday;
         name = line;
+        closest.emplace_back(name, birthday);
       }
     }
   }
   if (!bornToday.empty()) {
-    cout << "Today is the birthday of: ";
-    for (auto & i : bornToday) cout << i << " ";
+    cout << "Today is the birthday of: " << endl;
+    for (auto & i : bornToday) cout << i << endl;
+    cout << endl;
   }
-  cout << endl << "Closest birthday: " << name << ", " << put_time(&birthday, "%m/%d");
+  cout << "Closest birthdays: " << endl;
+  ul i = closest.size() - 1;
+  while (true) {
+    cout << closest[i].first << ", " << put_time(&closest[i].second, "%m/%d") << endl;
+    if (isEqual(closest[i].second, closest[i - 1].second) && i != 0)  i--;
+    else break;
+  }
   return 0;
 }
